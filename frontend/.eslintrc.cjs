27e1c30f5550a2df7a339a6c38f08ca409797029
Kellen Miller <kellen.miller12@gmail.com>
2022-07-09 20:22:30 -0500

    init commit with gitignore
    Signed-off-by: Kellen Miller <kellen.miller12@gmail.com>

---
+++ frontend/.eslintrc.cjs
@@ -0,0 +1,23 @@
+import typescript from 'typescript'
+
+module.exports = {
+	root: true,
+	parser: '@typescript-eslint/parser',
+	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier', 'square-svelte-store'],
+	plugins: ['svelte3', '@typescript-eslint'],
+	ignorePatterns: ['*.cjs'],
+	overrides: [{files: ['*.svelte'], processor: 'svelte3/svelte3'}],
+	settings: {
+		'svelte3/typescript': () => typescript
+	},
+	rules: {'square-svelte-store/use-square-svelte-store': 'error'},
+	parserOptions: {
+		sourceType: 'module',
+		ecmaVersion: 2020
+	},
+	env: {
+		browser: true,
+		es2017: true,
+		node: true
+	}
+};
