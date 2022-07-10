ff0a727910c563a3dcf8331cae5887d1aa0ca624
Kellen Miller <kellen.miller12@gmail.com>
2022-07-09 20:44:32 -0500

    init frontend
    Signed-off-by: Kellen Miller <kellen.miller12@gmail.com>

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -5,11 +5,9 @@ module.exports = {
 	parser: '@typescript-eslint/parser',
 	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier', 'square-svelte-store'],
 	plugins: ['svelte3', '@typescript-eslint'],
-	ignorePatterns: ['*.cjs'],
+	ignorePatterns: ['*.cjs']
 	overrides: [{files: ['*.svelte'], processor: 'svelte3/svelte3'}],
-	settings: {
-		'svelte3/typescript': () => typescript
-	},
+	settings: {'svelte3/typescript': () => typescript},
 	rules: {'square-svelte-store/use-square-svelte-store': 'error'},
 	parserOptions: {
 		sourceType: 'module',
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,13 @@ import {sveltekit} from '@sveltejs/kit/vite';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-    plugins: [sveltekit()]
+    plugins: [sveltekit()],
+    resolve: {
+        alias: {
+            $root: path.resolve('./src'),
+        },
+    },
+
 };
 
 export default config;
