540b88b66e1f114e0c72c40cdb3742230fbafe22
Kellen Miller <kellen.miller12@gmail.com>
2022-07-11 13:11:56 -0500

    pruned server of demo routes
    Signed-off-by: Kellen Miller <kellen.miller12@gmail.com>

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -32,8 +32,10 @@ use serde_json::{
     json,
     Value,
 };
-use tower::ServiceBuilder;
-use tower_http::cors::{Any, CorsLayer};
+use tower_http::cors::{
+    Any,
+    CorsLayer,
+};
 use tracing_subscriber::{
     fmt::layer,
     layer::SubscriberExt,
@@ -45,8 +47,6 @@ use crate::data::DATA;
 
 mod book;
 mod data;
-mod repo;
-mod repo2;
 
 #[tokio::main]
 pub async fn main() {
@@ -68,25 +68,6 @@ pub async fn main() {
         .allow_origin(Any)
         .allow_headers(Any);
 
-    // Create demo router
-    let demo_routes = Router::new()
-        .route("/demo.html", get(get_demo_html))
-        .route("/hello.html", get(hello_html))
-        .route("/demo-status", get(demo_status))
-        .route("/demo-uri", get(demo_uri))
-        .route("/demo.png", get(get_demo_png))
-        .route("/foo",
-               get(get_foo)
-                   .put(put_foo)
-                   .patch(patch_foo)
-                   .post(post_foo)
-                   .delete(delete_foo))
-        .route("/items/:id", get(get_items_id))
-        .route("/items", get(get_items))
-        .route("/demo.json",
-               get(get_demo_json)
-                   .put(put_demo_json));
-
     // Create books route
     let book_routes = Router::new()
         .route("/",
@@ -94,15 +75,11 @@ pub async fn main() {
                    .put(put_books))
         .route("/:id",
                get(get_books_id)
-                   .delete(delete_books_id))
-        .route("/:id/form",
-               get(get_books_id_form)
-                   .post(post_books_id_form));
+                   .delete(delete_books_id));
+
 
     // Create app router
     let app_routes = Router::new()
-        .route("/", get(hello))
-        .nest("/demo", demo_routes)
         .nest("/books", book_routes)
         .fallback(fallback.into_service());
     // .layer(ServiceBuilder::new().layer(cors_layer));
@@ -171,47 +148,6 @@ pub async fn get_books_id(Path(id): Path<u32>) -> Json<Book> {
     }).join().unwrap().into()
 }
 
-
-/// axum handler for "POST /books/:id/form" which submits an HTML form.
-/// This demo shows how to do a form submission then update a resource.
-pub async fn post_books_id_form(form: Form<Book>) -> Html<String> {
-    let new_book: Book = form.0;
-    thread::spawn(move || {
-        let mut data = DATA.lock().unwrap();
-        if data.contains_key(&new_book.id) {
-            data.insert(new_book.id, new_book.clone());
-            format!("<p>{}</p>\n", &new_book)
-        } else {
-            format!("Book id not found: {}", &new_book.id)
-        }
-    }).join().unwrap().into()
-}
-
-/// axum handler for "GET /books/:id/form" which responds with a form.
-/// This demo shows how to write a typical HTML form with input fields.
-pub async fn get_books_id_form(Path(id): Path<u32>) -> Html<String> {
-    thread::spawn(move || {
-        let data = DATA.lock().unwrap();
-        match data.get(&id) {
-            Some(book) => format!(
-                concat!(
-                "<form method=\"post\" action=\"/books/{}/form\">\n",
-                "<input type=\"hidden\" name=\"id\" value=\"{}\">\n",
-                "<p><input name=\"title\" value=\"{}\"></p>\n",
-                "<p><input name=\"author\" value=\"{}\"></p>\n",
-                "<input type=\"submit\" value=\"Save\">\n",
-                "</form>\n"
-                ),
-                &book.id,
-                &book.id,
-                &book.title,
-                &book.author
-            ),
-            None => format!("<p>Book id {} not found</p>", id),
-        }
-    }).join().unwrap().into()
-}
-
 /// axum handler for "PUT /books" which creates a new book resource.
 /// This demo shows how axum can extract JSON data into a Book struct.
 pub async fn put_books(Json(book): Json<Book>) -> Html<String> {
@@ -249,106 +185,4 @@ async fn shutdown_signal() {
         .await
         .expect("expect tokio signal ctrl-c");
     println!("signal shutdown");
-}
-
-
-/// axum handler for "PUT /demo.json" which uses `aumx::extract::Json`.
-/// This buffers the request body then deserializes it using serde.
-/// The `Json` type supports types that implement `serde::Deserialize`.
-pub async fn put_demo_json(Json(data): Json<Value>) -> String {
-    format!("Put demo JSON data: {:?}", data)
-}
-
-/// axum handler for "PUT /demo.json" which uses `aumx::extract::Json`.
-/// This buffers the request body then deserializes it bu using serde.
-/// The `Json` type supports types that implement `serde::Deserialize`.
-pub async fn get_demo_json() -> Json<Value> {
-    json!({"a":"b"}).into()
-}
-
-/// axum handler for "GET /items" which uses `axum::extract::Query`.
-/// This extracts query parameters and creates a key-value pair map.
-pub async fn get_items(Query(params): Query<HashMap<String, String>>) -> String {
-    format!("Get items with query params: {:?}", params)
-}
-
-/// axum handler for "GET /items/:id" which uses `axum::extract::Path`.
-/// This extracts a path parameter then deserializes it as needed.
-pub async fn get_items_id(Path(id): Path<String>) -> String {
-    format!("Get items with path id: {:?}", id)
-}
-
-/// axum handler for "GET /foo" which returns a string message.
-/// This shows our naming convention for HTTP GET handlers.
-pub async fn get_foo() -> String {
-    "GET foo".to_string()
-}
-
-/// axum handler for "PUT /foo" which returns a string message.
-/// This shows our naming convention for HTTP PUT handlers.
-pub async fn put_foo() -> String {
-    "PUT foo".to_string()
-}
-
-/// axum handler for "PATCH /foo" which returns a string message.
-/// This shows our naming convention for HTTP PATCH handlers.
-pub async fn patch_foo() -> String {
-    "PATCH foo".to_string()
-}
-
-/// axum handler for "POST /foo" which returns a string message.
-/// This shows our naming convention for HTTP POST handlers.
-pub async fn post_foo() -> String {
-    "POST foo".to_string()
-}
-
-/// axum handler for "DELETE /foo" which returns a string message.
-/// This shows our naming convention for HTTP DELETE handlers.
-pub async fn delete_foo() -> String {
-    "DELETE foo".to_string()
-}
-
-/// axum handler for "GET /" which returns a string and causes axum to
-/// immediately respond with status code `200 OK` and with the string.
-pub async fn hello() -> String {
-    "Hello, World!".into()
-}
-
-/// axum handler for "GET /demo.html" which responds with HTML text.
-/// The `Html` type sets an HTTP header content-type of `text/html`.
-pub async fn get_demo_html() -> Html<&'static str> {
-    "<h1>Hello</h1>".into()
-}
-
-/// axum handler that responds with typical HTML coming from a file.
-/// This uses the Rust macro `std::include_str` to include a UTF-8 file
-/// path, relative to `main.rs`, as a `&'static str` at compile time.
-async fn hello_html() -> Html<&'static str> {
-    include_str!("hello.html").into()
-}
-
-/// axum handler for "GET /demo-status" which returns a HTTP status
-/// code, such as OK (200), and a custom user-visible string message.
-pub async fn demo_status() -> (StatusCode, String) {
-    (StatusCode::OK, "Everything is OK".to_string())
-}
-
-/// axum handler for "GET /demo-uri" which shows the request's own URI.
-/// This shows how to write a handler that receives the URI.
-pub async fn demo_uri(uri: Uri) -> String {
-    format!("The URI is: {:?}", uri)
-}
-
-/// axum handler for "GET /demo.png" which responds with an image PNG.
-/// This sets a header "image/png" then sends the decoded image data.
-async fn get_demo_png() -> impl axum::response::IntoResponse {
-    let png = concat!(
-    "iVBORw0KGgoAAAANSUhEUgAAAAEAAAAB",
-    "CAYAAAAfFcSJAAAADUlEQVR42mPk+89Q",
-    "DwADvgGOSHzRgAAAAABJRU5ErkJggg=="
-    );
-    (
-        AppendHeaders([(header::CONTENT_TYPE, "image/png"), ]),
-        base64::decode(png).unwrap(),
-    )
 }
