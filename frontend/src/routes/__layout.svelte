10deefa94496a6c24064970be9d34e8a34d2aa69
Kellen Miller <kellen.miller12@gmail.com>
2022-07-17 13:28:12 -0500

    converting to svelte ui
    Signed-off-by: Kellen Miller <kellen.miller12@gmail.com>

--- frontend/src/routes/__layout.svelte
+++ frontend/src/routes/__layout.svelte
@@ -1,15 +1,72 @@
 <script lang="ts">
-    import '@/styles/app.css'
-    import Navbar from "@/components/nav/Navbar.svelte";
+    // import '@/styles/app.css'
+    // import Navbar from "@/components/nav/Navbar.svelte";
+    import {
+        ActionIcon,
+        AppShell,
+        Aside,
+        Footer,
+        Header,
+        Navbar,
+        ShellSection,
+        SvelteUIProvider
+    } from '@svelteuidev/core';
+    import {Sun} from "radix-icons-svelte";
 
     let stickToTop = true;
+    let isDark = false;
+    let opened = false;
+
+    function toggleTheme() {
+        isDark = !isDark;
+    }
+
+    function toggleOpened() {
+        opened = !opened;
+    }
 </script>
 
-{#if !stickToTop}
-    <slot/>
-{/if}
-<Navbar {stickToTop}/>
-{#if stickToTop}
-    <slot/>
-{/if}
+<SvelteUIProvider ssr themeObserver={isDark ? 'dark' : 'light'} withGlobalStyles withNormalizeCSS>
+    <AppShell
+          asideOffsetBreakpoint="sm"
+          fixed
+          navbarOffsetBreakpoint="sm"
+    >
+        <Navbar
+              fixed
+              hidden={!opened}
+              hiddenBreakpoint="sm"
+              override={{ p: '$mdPX' }}
+              slot="navbar"
+              width={{ sm: 200, lg: 250 }}
+        >
 
+            navbar content
+            <!--            <NavbarContent />-->
+        </Navbar>
+
+        <Header fixed height={60} override={{ display: 'flex', p: '$mdPX' }} slot="header">
+            <ActionIcon on:click={toggleTheme}>
+                <Sun/>
+            </ActionIcon>
+            header content
+            <!--            <HeaderContent />-->
+        </Header>
+
+        <!-- Main content uses the default slot, so no need to explicitly declare it -->
+        <ShellSection grow>
+            main content
+            <!--            <MainContent />-->
+        </ShellSection>
+
+        <Aside slot="aside" width={{ base: 200 }}>
+            aside content
+            <!--            <AsideContent />-->
+        </Aside>
+
+        <Footer slot="footer">
+            footer content
+            <!--            <FooterContent />-->
+        </Footer>
+    </AppShell>
+</SvelteUIProvider>
